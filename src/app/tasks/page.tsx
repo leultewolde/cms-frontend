b0d8b216988e40b9664d2bce099202b6090eac57
Leul Tewolde <leulwoldegabriel@gmail.com>
2024-06-22 11:25:41 -0500

    improved list of tasks component

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import {NextPage} from 'next';
-import Link from 'next/link';
 import TaskForm from '../../components/task/TaskForm';
 import withAuth from "@/components/withAuth";
 import useTasks from "@/hooks/useTasks";
@@ -8,22 +7,18 @@ import ListView from "@/components/ListView";
 
 const Tasks: NextPage = () => {
     const {tasks, setRefreshing} = useTasks();
-    // const [tasks, setTasks] = useState<TaskResponseDTO[]>([]);
-    //
-    // useEffect(() => {
-    //     async function fetchTasks() {
-    //         const data = await getTasks();
-    //         setTasks(data);
-    //     }
-    //     fetchTasks();
-    // }, []);
 
     return (
         <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
             <TaskForm setRefreshing={setRefreshing}/>
             <div>
                 <h1>Tasks</h1>
-                <ListView values={tasks.map(task => ({id: task.taskId, name: task.title, desc: task.description, href: `/tasks/${task.taskId}`}))}/>
+                <ListView values={tasks.map(task => ({
+                    id: task.taskId,
+                    name: `${task.title} (deadline: ${task.deadline.toString()})`,
+                    desc: task.description,
+                    href: `/tasks/${task.taskId}`
+                }))}/>
             </div>
         </div>
     );
